dd64b583fe13c672a219c692ea9b5ed154bdc7ce
Jon Zingale <jonzingale@gmail.com>
2020-04-11 17:50:37 -0600

    towards dropdowns

--- src/components/Navigation2.jsx
+++ src/components/Navigation2.jsx
@@ -6,36 +6,35 @@ import TestTemplate from './../pages/TestTemplate.jsx';
 import HaskellCoders from './../pages/HaskellCoders.jsx';
 import SvgTemplate from './../pages/SvgTemplate.jsx';
 
-class Navigation extends Component {
+class Navigation2 extends Component {
 
   render() {
-
-  // not yet any good.
-  function dropdown() {
-    return(
-      <NavDropdown title='HaskellCoders' href='/haskell-coding-group' id="basic-nav-dropdown">
-        <NavDropdown.Item href="#/action-1">Listable</NavDropdown.Item>
-        <NavDropdown.Item href="#/action-2">Sortable</NavDropdown.Item>
-        <NavDropdown.Item href="#/action-3">Vector</NavDropdown.Item>
-        <NavDropdown.Item href="#/action-3">Abelian Zippers</NavDropdown.Item>
-        <NavDropdown.Item href="#/action-3">HTF</NavDropdown.Item>
-        <NavDropdown.Item href="#/action-3">Umeboshi</NavDropdown.Item>
-        <NavDropdown.Item href="#/action-3">Havel-Hakimi</NavDropdown.Item>
-      </NavDropdown>
-    )
-  }
-
-    // <Nav.Link href='/lights-game'>Lights Game</Nav.Link>
     return (
       <div>
         <Router>
           <div>
-            <Navbar>
-              <Nav>
+            <Navbar expand="xl">
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" onClick='true'/>
+              <Navbar.Collapse id="navbar-collapse" onToggle='true'>
+              <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/haskell-coding-group">HaskellCoders</Nav.Link>
+                <NavDropdown title='HaskellCoders' id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#/action-1">Listable</NavDropdown.Item>
+                  <NavDropdown.Item href="#/action-2">Sortable</NavDropdown.Item>
+                  <NavDropdown.Item href="#/action-3">Vector</NavDropdown.Item>
+                  <NavDropdown.Item href="#/action-3">Abelian Zippers</NavDropdown.Item>
+                  <NavDropdown.Item href="#/action-3">HTF</NavDropdown.Item>
+                  <NavDropdown.Item href="#/action-3">Umeboshi</NavDropdown.Item>
+                  <NavDropdown.Item href="#/action-3">Havel-Hakimi</NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title='HaskellCoders2' id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#/action-1">Listable</NavDropdown.Item>
+                  <NavDropdown.Item href="#/action-2">Sortable</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href='/lights-game'>Lights Game</Nav.Link>
                 <Nav.Link href="/svg-template">SvgTemplate</Nav.Link>
               </Nav>
+              </Navbar.Collapse>
             </Navbar>
 
             {/* A <Switch> looks through its children <Route>s and
@@ -62,4 +61,4 @@ class Navigation extends Component {
   }
 }
 
-export default Navigation;
+export default Navigation2;
