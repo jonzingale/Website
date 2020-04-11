32097e8de47b84ab3194b79a53edf77c9699c565
Jon Zingale <jonzingale@gmail.com>
2020-04-11 17:19:20 -0600

    stub for dropdown nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Dropdown } from 'react-bootstrap';
 
 import TestTemplate from './../pages/TestTemplate.jsx';
 import HaskellCoders from './../pages/HaskellCoders.jsx';
@@ -9,6 +9,28 @@ import SvgTemplate from './../pages/SvgTemplate.jsx';
 class Navigation extends Component {
 
   render() {
+
+  // not yet any good.
+  function dropdown() {
+    return(
+      <Dropdown>
+        <Dropdown.Toggle variant='Primary'>
+         <Nav.Link href="/haskell-coding-group">HaskellCoders</Nav.Link>
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Listable</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Sortable</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Vector</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Abelian Zippers</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">HTF</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Umeboshi</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Havel-Hakimi</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    )
+  }
+
     return (
       <div>
         <Router>
@@ -16,7 +38,7 @@ class Navigation extends Component {
             <Navbar>
               <Nav>
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/haskell-coding-group">HaskellCoders</Nav.Link>
+                <Nav.Link href='/lights-game'>Lights Game</Nav.Link>
                 <Nav.Link href="/svg-template">SvgTemplate</Nav.Link>
               </Nav>
             </Navbar>
@@ -29,6 +51,9 @@ class Navigation extends Component {
               </Route>
               <Route path="/svg-template">
                 <SvgTemplate title='svg notebook'/>
+              </Route>
+              <Route path="/lights-game">
+                <SvgTemplate title='Wow Lights Game'/> {/* TODO: Migrate*/}
               </Route>
               <Route path="/" title='jonzingale@gmail.com'>
                 <TestTemplate />
