0292bcfbdc158bfd792743349fb7e8c5e66ead82
Jon Zingale <jonzingale@gmail.com>
2020-04-11 22:00:40 -0600

    steps toward explorable integration

---
+++ src/js/ExplorableMenu.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import * as d3 from 'd3'
+
+class ExplorableMenu extends Component {
+
+  componentDidMount() {
+    const yHeight = 50
+    const modelColors = [
+      '#8c510a','#bf812d','#dfc27d', '#FFD700',
+      '#80cdc1','#35978f','#01665e',
+      '#435084', '#2E48A5', '#613375',
+      '#9C3DF4'
+    ]
+
+    const data =
+      [ {'id': 'Contagion Network', 'url': "/explorables/epidemics/epidemics.html"},
+        {'id': 'Schelling Segregation', 'url': "/explorables/schelling/schelling.html"},
+        {'id': 'Nagel-Schreckenberg Traffic', 'url': "/explorables/nagelschreckenberg/nagelSchreckenberg.html"},
+        {'id': 'Cellular Automata', 'url': "/explorables/briansBrain/briansBrain.html"},
+        {'id': 'The Lorenz System', 'url': "/explorables/lorenz/lorenz.html"},
+        {'id': 'Ann Nitze Hiking Trails', 'url': "/maps/hiking.html"},
+        {'id': 'Deep Image Networks', 'url': 'neural-nets'},
+        {'id': 'Color Summaries', 'url': "/colorSummaries/index.html"},
+        {'id': 'Dynamical Monads', 'url': "/dynamical-monads/dynamical.html"},
+        {'id': 'Primitive Roots', 'url': "/galois/index.html"},
+        {'id': 'Covid 19 Visualizer', 'url': "/covid19/covid19.html"},
+      ]
+
+    var col = d3.select('#buttons')
+                 .attr('height', 550).attr('width', 470)
+
+    var modelLinks = col.selectAll("text").data(data).enter()
+
+    modelLinks.append('g')
+        .selectAll("circle").data(data)
+        .enter().append("circle")
+          .attr('id', function(d) { return d.id })
+          .attr("cx", 17).attr("r", 16)
+          .attr("cy", function(d, i) { return i*yHeight + 20 })
+          .attr('fill', function(d, i) { return modelColors[i] })
+
+    modelLinks.append('a')
+        .attr('href', function(d) { return d.url })
+        .append('text').attr('class', 'menu').attr('x', 43)
+        .attr('y', function(d, i) { return i*yHeight + 31})
+        .attr('font-family', 'Nunito Sans')
+        .attr("font-size", "30px")
+        .text(function(d) {return d.id})
+  }
+
+  render(){
+    return(<svg id='buttons' width='100%'></svg>)
+  }
+}
+
+export default ExplorableMenu;
---
+++ src/svg/PlumBlossomFull.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+import * as d3 from 'd3'
+
+class PlumBlossom extends Component {
+
+  componentDidMount() {
+    var svg = d3.selectAll('#plum-blossom')
+    var svg_width = svg.style('width').replace('px','')
+    var svg_height = svg.style('height').replace('px','')
+
+    var xScale = d3.scaleLinear()
+      .domain([0, 1]).range([0, svg_width])
+
+    var yScale = d3.scaleLinear()
+      .domain([0, 1]).range([0, svg_height])
+
+    // background box
+    svg.append('rect')
+      .attr('class', 'background-box')
+      .attr('x', 1).attr('y', 0)
+      .attr('stroke','#777')
+      .attr('stroke-width', this.props.border ? '0.3em' : '0')
+      .attr('fill', this.props.paperColor) // cream paper
+      .attr('width','100%')
+
+    var lineGenerator = d3.line().curve(d3.curveBasis)
+    var pathData, points
+
+    // abstract line
+    points = [[0, 360],[100, 400],[300, 380],[700, 340],[svg_width, 380]];
+    pathData = lineGenerator(points);
+    svg.append('path').attr('d', pathData)
+    .attr('stroke', 'black')
+    .attr('fill', 'none')
+
+    // tree lines
+    lineGenerator = d3.line()
+    points = [[700, 440], [680, 280], [500, 220], [400, 300]];
+    pathData = lineGenerator(points);
+    svg.append('path').attr('d', pathData)
+    .attr('stroke', '#2c202f')
+    .attr('stroke-width', 30)
+    .attr('fill', 'none')
+
+    points = [[710, 438], [690, 280]];
+    pathData = lineGenerator(points);
+    svg.append('path').attr('d', pathData)
+    .attr('stroke', '#6f4891')
+    .attr('stroke-width', 10)
+    .attr("opacity", 0.5)
+
+    points = [[520, 280], [700, 180], [500, 110]];
+    pathData = lineGenerator(points);
+    svg.append('path').attr('d', pathData)
+    .attr('stroke', '#23242b')
+    .attr('stroke-width', 17)
+    .attr('fill', 'none')
+
+    points = [[680, 280], [400, 160], [230, 300]];
+    pathData = lineGenerator(points);
+    svg.append('path').attr('d', pathData)
+    .attr('stroke', '#23242b')
+    .attr('stroke-width', 17)
+    .attr('fill', 'none')
+
+    // tree circles
+    var data = []
+    for (let i=0; i < 300; i++) { data.push(i)}
+
+    svg.append("g")
+      .attr("class", "blue-circles")
+      .selectAll("circle")
+      .data(data).enter().append("circle")
+        .attr('id', function(d, i) { return i })
+        .attr("r", function(d) { return Math.random()*30 })
+        .attr('cy', function(d) { return yScale(Math.random())/2.2 + 40})
+        .attr('cx', function(d) { return xScale(Math.random())/1.3 + svg_width/7 })
+        .attr('fill', function(d) { return d3.interpolateBlues(d/1200) })
+        .attr("opacity", function(d) { return Math.random() + 0.7 })
+        .attr('stroke-width', '1.5px')
+        .style("filter", d => d < 200 ? "url(#blur)" : ""); // blur
+
+    // blur
+    var defs = svg.append("defs")
+    defs.append("defs")
+      .append("filter")
+      .attr("id", "blur")
+      .append("feGaussianBlur")
+      .attr("stdDeviation", 3)
+
+    data = []
+    for (let i=0; i < 150; i++) { data.push(i)}
+
+    svg.append("g")
+      .attr("class", "pink-circles")
+      .selectAll("circle")
+      .data(data).enter().append("circle")
+        .attr('id', function(d, i) { return i })
+        .attr("r", 3)
+        .attr('cy', function(d) { return yScale(Math.random())/2.2 + 40})
+        .attr('cx', function(d) { return xScale(Math.random())/1.4 + svg_width/6 })
+        .attr('fill', function(d) { return d3.interpolateReds(d/200) })
+        .attr('stroke-width', '1.5px')
+  }
+
+  render(){
+    return(<svg id='plum-blossom' width='100%' height='600px'></svg>)
+  }
+}
+
+export default PlumBlossom;
