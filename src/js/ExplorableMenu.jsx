b2195cab60115eeb2a69fb6bdc8f15e4fe849ac2
Jon Zingale <jonzingale@gmail.com>
2020-04-12 17:55:43 -0600

    links for explorable menu

--- src/js/ExplorableMenu.jsx
+++ src/js/ExplorableMenu.jsx
@@ -4,13 +4,19 @@ import * as d3 from 'd3'
 class ExplorableMenu extends Component {
 
   componentDidMount() {
-    const yHeight = 50
+    const yHeight = 60
     const modelColors = [
       '#8c510a','#bf812d','#dfc27d', '#FFD700',
-      '#80cdc1','#35978f','#01665e',
-      '#435084', '#2E48A5', '#613375',
+      '#80cdc1',
+      '#35978f',
+      '#01665e',
+      '#435084',
+      // '#2E48A5',
+      '#613375',
       '#9C3DF4'
     ]
+
+    const urlStub = 'https://jonzingale.github.io'
 
     const data =
       [ {'id': 'Contagion Network', 'url': "/explorables/epidemics/epidemics.html"},
@@ -18,12 +24,12 @@ class ExplorableMenu extends Component {
         {'id': 'Nagel-Schreckenberg Traffic', 'url': "/explorables/nagelschreckenberg/nagelSchreckenberg.html"},
         {'id': 'Cellular Automata', 'url': "/explorables/briansBrain/briansBrain.html"},
         {'id': 'The Lorenz System', 'url': "/explorables/lorenz/lorenz.html"},
-        {'id': 'Ann Nitze Hiking Trails', 'url': "/maps/hiking.html"},
-        {'id': 'Deep Image Networks', 'url': 'neural-nets'},
-        {'id': 'Color Summaries', 'url': "/colorSummaries/index.html"},
+        // {'id': 'Ann Nitze Hiking Trails', 'url': "/maps/hiking.html"},
         {'id': 'Dynamical Monads', 'url': "/dynamical-monads/dynamical.html"},
         {'id': 'Primitive Roots', 'url': "/galois/index.html"},
         {'id': 'Covid 19 Visualizer', 'url': "/covid19/covid19.html"},
+        {'id': 'Deep Image Networks', 'url': 'neural-nets'},
+        // {'id': 'Color Summaries', 'url': "/colorSummaries/index.html"},
       ]
 
     var col = d3.select('#buttons')
@@ -36,13 +42,13 @@ class ExplorableMenu extends Component {
         .enter().append("circle")
           .attr('id', function(d) { return d.id })
           .attr("cx", 17).attr("r", 16)
-          .attr("cy", function(d, i) { return i*yHeight + 20 })
+          .attr("cy", function(d, i) { return i*yHeight + 30 })
           .attr('fill', function(d, i) { return modelColors[i] })
 
     modelLinks.append('a')
-        .attr('href', function(d) { return d.url })
+        .attr('href', function(d) { return urlStub+d.url })
         .append('text').attr('class', 'menu').attr('x', 43)
-        .attr('y', function(d, i) { return i*yHeight + 31})
+        .attr('y', function(d, i) { return i*yHeight + 41})
         .attr('font-family', 'Nunito Sans')
         .attr("font-size", "30px")
         .text(function(d) {return d.id})
