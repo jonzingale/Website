ab28ba2ddb06059a1111ed8138095f478f0336f8
Jon Zingale <jonzingale@gmail.com>
2020-04-11 22:34:59 -0600

    header title

--- src/pages/Explorable.jsx
+++ src/pages/Explorable.jsx
@@ -2,20 +2,20 @@ import React, { Component } from 'react'
 import PlumBlossom from './../svg/PlumBlossomFull.jsx'
 import ExplorableMenu from './../js/ExplorableMenu.jsx'
 import './../css/main.css'
-import './../css/full-page.css' // WHY DOES THIS FUCK WITH EVERYTHING???
+import './../css/full-page.css'
 
 class Explorable extends Component {
   render() {
     return(
       <div className='explorable'>
         <div className='container'>
-          <header>.</header>
+          <header>D3 projects</header>
           <article>
             <ExplorableMenu />
           </article>
           <aside className='sidebar-right'>
             <div className='sidebar-content'>
-              <PlumBlossom paperColor={'#f4f2e9'}/>
+              <PlumBlossom />
             </div>
           </aside>
         </div>
